0512831bc48c8ff9b1789ba55d077ff0bee3696d
jangyunhwan <jangyunhwan@gmail.com>
2022-01-03 22:05:40 +0900

    Add login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,38 @@
 <template>
-    <div>
-        <h1>About Login!</h1>
-    </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 >
+            <v-card class="elevation-12">
+              <v-toolbar dark color="dark">
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field name="Email" label="Email" type="text"></v-text-field>
+                  <v-text-field id="Password" name="Password" label="Password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+
 <script>
-export default {
-    
-}
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
 </script>
